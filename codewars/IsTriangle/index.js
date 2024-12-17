841a956aedb0813a6e334baa10df11e3b08f6cb3
enyllaoliveira <enyllaoliveira@gmail.com>
2024-12-17 07:45:53 -0300

    a solution to te is a triangle problem fixed

--- codewars/IsTriangle/index.js
+++ codewars/IsTriangle/index.js
@@ -14,10 +14,6 @@
 // 1,2,9 -> false
 
 function isTriangle(a, b, c) {
-  if (a <= 0 && b <= 0 && c <= 0) {
-    return false;
-  }
-
   if (a + b > c && a + c > b && b + c > a) {
     return true;
   } else {
