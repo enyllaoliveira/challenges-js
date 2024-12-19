6633a91f5c6a508349ad4db9797a7392c6c84174
enyllaoliveira <enyllaoliveira@gmail.com>
2024-12-19 11:06:27 -0300

    sollution of problem related to sum one from to num  added

--- codewars/Sum1FromToNum/index.js
+++ codewars/Sum1FromToNum/index.js
@@ -7,7 +7,11 @@
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
 var summation = function (num) {
-  // Code here
+  let numSum = 0;
+  for (let i = 1; i <= num; i++) {
+    numSum += i;
+  }
+  return numSum;
 };
 
 module.exports = summation;
