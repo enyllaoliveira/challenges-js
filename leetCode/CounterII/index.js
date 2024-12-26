f6bdad77201bc1e4ca0ac410341d0eaf1d455377
enyllaoliveira <enyllaoliveira@gmail.com>
2024-12-26 10:06:36 -0300

    description to to counter II problem added

--- leetCode/CounterII/index.js
+++ leetCode/CounterII/index.js
@@ -5,3 +5,27 @@
 // increment() increases the current value by 1 and then returns it.
 // decrement() reduces the current value by 1 and then returns it.
 // reset() sets the current value to init and then returns it.
+
+function createCounter(init) {
+  if (typeof init !== "number") {
+    throw new Error("Input must be a number");
+  }
+
+  let current = init;
+  return {
+    increment: function (createCounter) {
+      current++;
+      return current;
+    },
+    decrement: function (createCounter) {
+      current--;
+      return current;
+    },
+    reset: function (createCounter) {
+      current = init;
+      return current;
+    },
+  };
+}
+
+module.exports = createCounter;
