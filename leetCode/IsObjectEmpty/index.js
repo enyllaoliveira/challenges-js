d700a5e1917b9cb2e4dae44317fb46ba2493c5e9
enyllaoliveira <enyllaoliveira@gmail.com>
2025-01-03 15:17:23 -0300

    a solution to is objec empty problem added

--- leetCode/IsObjectEmpty/index.js
+++ leetCode/IsObjectEmpty/index.js
@@ -4,3 +4,15 @@
 // An empty object contains no key-value pairs.
 // An empty array contains no elements.
 // You may assume the object or array is the output of JSON.parse.
+
+var isEmpty = function (obj) {
+  if (obj == null) return true;
+
+  if (typeof obj === "object") {
+    return Object.keys(obj).length === 0;
+  }
+  if (Array.isArray(obj)) {
+    return obj.lengh === 0;
+  }
+  return false;
+};
