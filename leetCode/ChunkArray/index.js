5a1e70bd6b5b23f2d705f7e444a3b439597d036b
enyllaoliveira <enyllaoliveira@gmail.com>
2025-01-08 11:19:29 -0300

    feat: a solution to chunk array problem

--- leetCode/ChunkArray/index.js
+++ leetCode/ChunkArray/index.js
@@ -8,3 +8,14 @@
 // Input: arr = [1,2,3,4,5], size = 1
 // Output: [[1],[2],[3],[4],[5]]
 // Explanation: The arr has been split into subarrays each with 1 element.
+
+function chunk(arr, size) {
+  let chunks = [];
+  for (let i = 0; i < arr.length; i += size) {
+    const chunk = arr.slice(i, i + size);
+    chunks.push(chunk);
+  }
+  return chunks;
+}
+
+module.exports = chunk;
