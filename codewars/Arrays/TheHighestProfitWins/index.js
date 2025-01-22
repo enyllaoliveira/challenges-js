a56bbab0774fdb8fd3d9b7dc5defa2e63f746ef1
enyllaoliveira <enyllaoliveira@gmail.com>
2025-01-22 11:01:59 -0300

    feat: a solution of highest profit problem

--- codewars/Arrays/TheHighestProfitWins/index.js
+++ codewars/Arrays/TheHighestProfitWins/index.js
@@ -8,3 +8,17 @@
 // [1]         --> [1,1]
 
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+
+function minMax(arr) {
+  const { min, max } = arr.reduce(
+    (acc, value) => {
+      return {
+        min: Math.min(acc.min, value),
+        max: Math.max(acc.max, value),
+      };
+    },
+    { min: Infinity, max: -Infinity }
+  );
+
+  return [min, max];
+}
