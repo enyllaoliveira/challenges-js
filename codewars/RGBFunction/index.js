b41e761dfe33fb0fd5d4158b1061d5e890346490
enyllaoliveira <enyllaoliveira@gmail.com>
2025-02-16 09:27:26 -0300

    a instruction to RGB function added

---
+++ codewars/RGBFunction/index.js
@@ -0,0 +1,4 @@
+// RGB To Hex Conversion
+// Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+// Example (input --> output): 255, 255, 255 --> "FFFFFF"
+
