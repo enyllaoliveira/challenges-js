3a48c571c8ccbe48d47214ec85d1417042796326
enyllaoliveira <enyllaoliveira@gmail.com>
2025-02-16 09:27:48 -0300

    a solution to RGB function added

--- codewars/RGBFunction/index.js
+++ codewars/RGBFunction/index.js
@@ -2,3 +2,11 @@
 // Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
 // Example (input --> output): 255, 255, 255 --> "FFFFFF"
 
+function rgb(r, g, b) {
+  
+    limit = n => Math.max(0, Math.min(255, n))
+    
+    return [limit(r), limit(g), limit(b)]
+      .map(n => ("00" + n.toString(16)).slice(-2).toUpperCase()).join('')
+      return rgb;
+}
