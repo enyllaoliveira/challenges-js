17ac5166c0f2dca57fcdc0c00a25533a9a9d456f
enyllaoliveira <enyllaoliveira@gmail.com>
2025-02-16 10:20:23 -0300

    feat: a solution and a test  to range extraction problem

--- codewars/RangeExtraction/index.js
+++ codewars/RangeExtraction/index.js
@@ -8,3 +8,30 @@
 
 // solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
 // // returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+function solution(list){
+ 
+    let res = [];
+     let start = list[0];
+     let end = start;
+     
+     for (let i = 1; i <= list.length; i++) {
+       if (list[i] === end + 1) {
+         end = list[i]
+       } else {
+         if (end - start >= 2) {
+           res.push(`${start}-${end}`)
+         } else {
+           res.push(start.toString())
+           if (end !== start) res.push(end.toString())
+         }
+         start = list[i];
+         end = list[i];
+       }
+   
+     }
+     return res.join(",");
+   
+   }
+  
+  module.exports = solution;
---
+++ codewars/RangeExtraction/index.test.js
@@ -0,0 +1,5 @@
+const solution = require("./index.js");
+
+test("should to create a solution function", () => {
+  expect(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])).toBe('-10--8,-6,-3-1,3-5,7-11,14,15,17-20');
+});
